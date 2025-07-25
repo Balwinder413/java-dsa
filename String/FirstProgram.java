4cf3401735a04611c23d433b8a7c72ba8c54d694
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 14:11:48 +0530

    Add FirstProgram

---
+++ String/FirstProgram.java
@@ -0,0 +1,11 @@
+// declare
+public class FirstProgram {
+
+   public static void main(String[] var0) {
+      String name= "ballu";
+      String fullName = "balwinder kaur";
+      String sentence = "champo ka bna pampo ";
+      System.out.println(name);
+      System.out.println(sentence);
+   }
+}
