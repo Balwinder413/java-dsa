6a56b0d9578b7d68783f1a794aa40e9b9f4f84d9
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 20:07:38 +0530

    greeting messages helo

---
+++ Function/Greeting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Greeting{
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet(){
+        System.out.println("helo");
+    }
+}
