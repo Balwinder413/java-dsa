69e6c44e7229e4c73a707b0cc2c9b56b568d6a07
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 18:34:25 +0530

    Stringbuilder function

---
+++ String/StringMethod.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class StringMethod{
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("veerji");
+        System.out.println(sb);
+    
+    // charAt index 0
+    System.out.println(sb.charAt(0));
+
+
+    // set charAt index0
+    // sb.setCharAt(0, 'P');
+    // System.out.println(sb);
+
+    // set charAt 
+    // sb.setCharAt(4, 'g');
+    // System.out.println(sb);
+
+    // insert
+    sb.insert(0, 'b');
+    System.out.println(sb);
+
+    // delete b
+    sb.delete(0,2);
+    System.out.println(sb);
+    System.out.println(sb.length());
+}
+
+}
