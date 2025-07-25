bdf5a5a20ec9e8ba3f37f2a0b7b5091e8c876c56
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 17:23:11 +0530

    finding substring

---
+++ String/SubString.java
@@ -0,0 +1,8 @@
+     import java.util.*;
+ public class SubString {
+  public static void main(String[] args) {
+    String sentence="my name is ballu";
+    String name=sentence.substring(3,sentence.length());
+    System.out.println(name);
+  }
+}
