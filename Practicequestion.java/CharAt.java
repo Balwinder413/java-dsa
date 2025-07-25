84384c78f5442ecb64e1d99c455198f99319a876
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 11:02:11 +0530

    Create CharAt.java

---
+++ Practicequestion.java/CharAt.java
@@ -0,0 +1,8 @@
+public class CharAt {
+    public static void main(String[] args) {
+        System.out.println("Print chart");
+
+        String ch = "Ranjeet";
+        System.out.println(ch.charAt(0));
+    }
+}
