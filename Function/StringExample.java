62041ddc42a97cfd38e8548a4e782542ff780dbc
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 21:09:51 +0530

    return a greet messages

---
+++ Function/StringExample.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringExample{
+    public static void main(String[] args) {
+        String ans =greet();
+        System.out.println(ans);
+    }
+
+static String greet(){
+     String greet =" balwinder kaur";
+     return greet;
+}
+}
