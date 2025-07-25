8c8cd4d6c691471053d9eebb6b135a2b59b6da10
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 20:52:15 +0530

    return the value �

---
+++ Function/Sums.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Sums{
+    public static void main(String[] args){
+        int result =Sum();
+            System.out.println(result);
+        }
+
+    
+        static int Sum() {
+        Scanner sc = new Scanner(System.in);
+        int x= sc.nextInt();
+        int y = sc.nextInt();
+        int sum= x+y;
+        return sum;
+        }
+        }
+    
+
+    // Return type
+
+    // static int sum () {
+    //     Scanner in = new Scanner(System.in);
+    //     int num1 = in.nextInt();
+    //     int num2= in.nextInt();
+    //     int sum  = num1 + num2;
+    //     return sum;
+    // }
+
