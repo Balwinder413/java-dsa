eb229842389bbfd830d72284d06f1fe7de24d172
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 17:12:53 +0530

    Campare two strings

---
+++ String/Campare.java
@@ -0,0 +1,18 @@
+//compare
+  import java.util.*;
+ public class Campare{
+  public static void main(String[] args) {
+
+   String name1="veer ji";
+   String name2="bhaiya";
+
+    if(name1.compareTo(name2)==0){
+      System.out.println("string equal");
+    } else {
+      System.out.println("not equal");
+      }
+  }
+}
+//  s1>s2=+ve value
+//     s1 == s2= 0
+//     1<s2=-ve value
