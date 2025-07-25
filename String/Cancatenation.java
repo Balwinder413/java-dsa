18610d81e921a092ba2a5c3bccd30b95fa815a8e
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 17:10:13 +0530

    Cancate string

---
+++ String/Cancatenation.java
@@ -0,0 +1,13 @@
+// concatenation 
+ import java.util.*;
+ public class Cancatenation{
+   public static void main(String[] args) {
+      String name="veer";
+      String lastName="ji";
+      String fullName= name+ " "+lastName;
+      System.out.println(fullName);
+      System.out.println(fullName.length());
+  
+   }
+}
+
