a2b4c4b122297426b098a6a1afe28ca22d65b57b
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 15:12:47 +0530

    User from input

---
+++ String/Input.java
@@ -0,0 +1,9 @@
+// user input
+import java.util.*;
+ public class Input{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("my name is:"+ name);
+    }
+ }
