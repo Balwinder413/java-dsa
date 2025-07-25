7542baf93b8e4acb2d8be8f8e5a9557a5a43102d
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-25 10:37:25 +0530

    Add FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class FirstProgram{
+        public static void greet(){
+            System.out.println("hello");
+        }
+    public static void main(String[] args) {
+            greet();
+    }
+
+}
+    
+
