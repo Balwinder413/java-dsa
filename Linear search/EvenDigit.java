ae0761088fec774f35baac045f6c9cf07f1c4ee9
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-27 18:41:35 +0530

    count even digit

---
+++ Linear search/EvenDigit.java
@@ -0,0 +1,34 @@
+public class EvenDigit{
+    public static void main(String[] args) {
+        int[] nums={23,45,789,999,990,2324};{
+            System.out.println(findNumber(nums));
+        }
+
+    }
+    static int findNumber(int[] nums){
+        int i=0;
+     for(int num:nums){
+        if(even(num)){
+            i++;
+        }
+     }
+     return i;
+    }
+    static boolean even(int num){
+       int ans= Count(num);
+        if(ans % 2 == 0){
+            return true;
+        }
+        return false;
+        
+       
+    }
+ static int Count(int num){
+    int i=0;
+    while(num >0){
+        num= num/10;
+        i++;
+    }
+    return i;
+ }
+}
