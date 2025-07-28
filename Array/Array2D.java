864f284edb8bb61bb747e2405f1307027480716f
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-28 18:54:24 +0530

    input 2D array

---
+++ Array/Array2D.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Array2D{
+    public static void main(String[] args) {
+        int[][] arr=new int[4][2];
+        // syntax of 2d array
+        // int[][] nums= new int[][];
+    //     int [][] arr2D={
+    //         {1,2,3}  0th inex
+    //         {4,5,6}  1st index
+    //         {7,8,9}    2nd index
+    //     };
+          Scanner sc= new Scanner(System.in);
+
+          /********input*********/
+
+        for(int row=0; row<arr.length; row++){
+            for(int col=0; col<arr[row].length; col++){
+                arr[row][col]= sc.nextInt();
+               
+            }
+        }
+
+        /*****output */
+      for(int[]a: arr){
+        System.out.println(Arrays.toString(a));
+}
+        
+      }
+}
+
+       /***********output************/
+
+    //      for(int row=0; row<arr.length; row++){
+    //         for(int col=0; col<arr[row].length; col++){
+    //             System.out.print(arr[row][col] + " ");
+    //         }
+    //         System.out.println();
+        
+    //     }
+    //     System.out.println(Arrays.toString(arr[row][col]));
+    // }
+
+
+   /********output******/
+//          for(int row=0; row<arr.length; row++){
+//           System.out.println(Arrays.toString(arr[row]));
+// }
+
