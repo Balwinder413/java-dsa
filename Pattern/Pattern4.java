355d7a0176c0a4d55568d1698a4381dcb86f766e
Balwinder kaur <balwinderkaur1344@gmail.com>
2025-08-13 21:02:50 +0530

    pattern4 done

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,13 @@
+public class Pattern4{
+    public static void main(String[] args) {
+        print(4);
+    }
+    static void print(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(col );
+            }
+            System.out.println();
+        }
+    }
+}
