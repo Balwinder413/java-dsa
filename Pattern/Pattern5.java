a83c3c6c98ea896e3b2a02aa565dc544c528b032
Balwinder kaur <balwinderkaur1344@gmail.com>
2025-08-14 20:38:49 +0530

    pattern5 done

--- Pattern/Pattern5.java
+++ Pattern/Pattern5.java
@@ -6,7 +6,7 @@ public class Pattern5{
     }
     static void print(int n){
         for(int row=1; row< 2*n;row++){
-            int colInRow=row>n ?2*n-row-1:row;
+            int colInRow=row>n ?2*n-row:row;
                 for(int col=1;col<colInRow;col++){
                     System.out.print("* ");
                 }
