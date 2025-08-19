5186e561cd739a09a5db497699d70f88f76e30b0
Balwinder kaur <balwinderkaur1344@gmail.com>
2025-08-19 14:28:15 +0530

    find peak element

---
+++ Binary search/Peak.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Peak{
+    public static void main(String[] args) {
+        int[] arr={2,3,1,0};
+        int ans=peakvalue(arr);
+        System.out.println(ans);
+    }
+    static int  peakvalue(int[]arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+            return start;   // return end
+        }
+    }
