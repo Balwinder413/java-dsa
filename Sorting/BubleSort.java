e4ce35bd72dd973e665e94acec3db74d78ea7e9d
Balwinder413 <balwinderghangas99@gmail.com>
2025-08-31 19:27:37 +0530

    Update BubleSort.java

--- Sorting/BubleSort.java
+++ Sorting/BubleSort.java
@@ -1,21 +1,20 @@
 import java.util.*;
-public class BubleSort{
+public class Buble{
     public static void main(String[] args) {
-        int[]arr={8,7,4,0,4};
-        search(arr);
+        int[]arr={5,7,8,2,1,4,5};
+        bublesort(arr);
         System.out.println(Arrays.toString(arr));
-        
     }
-    static void  search(int []arr){
-        boolean swapped;
-        for(int i=0; i< arr.length;i++){
-            swapped =false;
-            for(int j=1; j<arr.length-i; j++){
-                if(arr[j]<arr[j-1]){
-                    int temp= arr[j];
+    static void bublesort(int[]arr){
+        for(int i=0; i<arr.length;i++){
+            boolean swapped=false;
+            for(int j=1;j<arr.length-i;j++){
+            
+                if(arr[j-1]>arr[j]){
+                    int temp=arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
-                    swapped =true;
+                    swapped=true;
                 }
             }
             if(!swapped){
