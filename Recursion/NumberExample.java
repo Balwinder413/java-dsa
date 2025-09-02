3696ece32269849b3f0b849ef8c28eac1101fa0d
Balwinder kaur <balwinderkaur1344@gmail.com>
2025-09-02 19:52:39 +0530

    first program

---
+++ Recursion/NumberExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class NumberExample{
+    public static void main(String[] args) {
+        print(1);
+    }
+    static void print(int n){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        // recursive call
+                print(n+1);
+    }
+}
